ad3fab3c671e5d24107a5e25a8316ddb8176671b
Brandon Erbschloe <berbschloe@gmail.com>
2021-05-07 11:22:21 -0500

    Initial Commit

---
+++ FlexView/FlexView.h
@@ -0,0 +1,18 @@
+//
+//  FlexView.h
+//  FlexView
+//
+//  Created by Brandon Erbschloe on 5/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexView.
+FOUNDATION_EXPORT double FlexViewVersionNumber;
+
+//! Project version string for FlexView.
+FOUNDATION_EXPORT const unsigned char FlexViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexView/PublicHeader.h>
+
+
