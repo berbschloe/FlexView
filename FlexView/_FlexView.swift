dfe67617be9a2b89bbf063059487438ae04431ae
Brandon Erbschloe <berbschloe@gmail.com>
2022-01-19 15:29:31 -0600

    Fix layout issues with spacing

--- FlexView/_FlexView.swift
+++ FlexView/_FlexView.swift
@@ -40,13 +40,15 @@ struct _FlexView<Data: Collection, Content: View>: View where Data.Element: Hash
 
         for element in data {
             let elementSize = elementsSize[element, default: CGSize(width: availableWidth, height: 0)]
-
+            var spacing = rows[currentRow].count != 0 ? self.spacing : 0
+            
             if remainingWidth - (elementSize.width + spacing) >= 0 {
                 rows[currentRow].append(element)
             } else {
                 currentRow += 1
                 rows.append([element])
                 remainingWidth = availableWidth
+                spacing = 0
             }
 
             remainingWidth -= (elementSize.width + spacing)
